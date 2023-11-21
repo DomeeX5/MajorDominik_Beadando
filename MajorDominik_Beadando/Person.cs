91c863a3d995c8884013a802c815110f35785d54
Petrik Diak <diak@petrik.hu>
2023-11-21 11:25:54 +0100

    Add project files.

---
+++ MajorDominik_Beadando/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MajorDominik_Beadando
+{
+	public class Person
+	{
+		[JsonProperty("id")]
+		public int Id { get; set; }
+		[JsonProperty("Email")]
+		public string Email { get; set; }
+		[JsonProperty("Full Name")]
+		public string Name { get; set; }
+		[JsonProperty("Phone Number")]
+		public string Phone { get; set; }
+
+		public override string ToString()
+		{
+			return $"Azonosító: {this.Id}\nNév: {this.Name}\nEmail: {this.Email}\nTelefonszám: {this.Phone}";
+		}
+	}
+
+}
