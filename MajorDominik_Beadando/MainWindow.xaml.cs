d15869ffe8c3ec3166545a22f3f51592e7dd4e56
Petrik Diak <diak@petrik.hu>
2023-11-23 08:41:30 +0100

    asdf

--- MajorDominik_Beadando/MainWindow.xaml.cs
+++ MajorDominik_Beadando/MainWindow.xaml.cs
@@ -29,17 +29,52 @@ namespace MajorDominik_Beadando
 
 		private void Add_Click(object sender, RoutedEventArgs e)
 		{
-			
+			PersonForm personForm = new PersonForm();
+			personForm.Closed += (_, _) =>
+			{
+				peopleList.ItemsSource = service.GetAll();
+			};
+			personForm.ShowDialog();
 		}
 
 		private void Delete_Click(object sender, RoutedEventArgs e)
 		{
-
+			Person selected = peopleList.SelectedItem as Person;
+			if (selected == null)
+			{
+				MessageBox.Show("A törléshez jelölj ki egy elemet!", "Hiba", MessageBoxButton.OK);
+				return;
+			}
+			MessageBoxResult result = MessageBox.Show($"Biztos, hogy törölni szeretnéd ezt az embert: {selected.Name}?", "Biztos?", MessageBoxButton.YesNo);
+			if (result == MessageBoxResult.Yes)
+			{
+				if (service.Delete(selected))
+				{
+					MessageBox.Show("Sikeres törlés!");
+				}
+				else
+				{
+					MessageBox.Show("Hiba történt törlés során!");
+					return;
+				}
+				peopleList.ItemsSource = service.GetAll();
+			}
 		}
 
 		private void Update_Click(object sender, RoutedEventArgs e)
 		{
-
+			Person selected = peopleList.SelectedItem as Person;
+			if (selected == null)
+			{
+				MessageBox.Show("A módosításhoz jelölj ki egy elemet!", "Hiba", MessageBoxButton.OK);
+				return;
+			}
+			PersonForm personForm = new PersonForm();
+			personForm.Closed += (_, _) =>
+			{
+				peopleList.ItemsSource = service.GetAll();
+			};
+			personForm.ShowDialog();
 		}
 	}
 }
--- MajorDominik_Beadando/PersonForm.xaml.cs
+++ MajorDominik_Beadando/PersonForm.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MajorDominik_Beadando;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,99 @@ namespace MajorDominik_Beadando
 	/// </summary>
 	public partial class PersonForm : Window
 	{
+		PeopleService service = new PeopleService();
+		Person person;
 		public PersonForm()
 		{
 			InitializeComponent();
+		}
+
+		public PersonForm(Person person)
+		{
+			InitializeComponent();
+			this.person = person;
+			LoadPerson();
+			buttonAdd.Visibility = Visibility.Collapsed;
+			buttonUpdate.Visibility = Visibility.Visible;
+		}
+
+		private void LoadPerson()
+		{
+			textBoxNev.Text = this.person.Name;
+			textBoxEmail.Text= this.person.Email;
+			textBoxTelefonszam.Text = this.person.Phone.ToString();
+		}
+
+		private void Add_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				Person person = CreatePersonFromInputFields();
+				Person newPerson = service.Add(person);
+				if (newPerson.Id != 0)
+				{
+					MessageBox.Show("Sikeres felvétel");
+					textBoxNev.Text = "";
+					textBoxEmail.Text = "";
+					textBoxTelefonszam.Text = "";
+				}
+				else
+				{
+					MessageBox.Show("Hiba történt a felvétel során");
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+
+		}
+
+		private void Update_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				Person person = CreatePersonFromInputFields();
+				Person updated = service.Update(person);
+				if (updated.Id != 0)
+				{
+					MessageBox.Show("Sikeres módosítás");
+
+				}
+				else
+				{
+					MessageBox.Show("Hiba történt a módosítás során");
+				}
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+
+		}
+
+		private Person CreatePersonFromInputFields()
+		{
+			string name = textBoxNev.Text.Trim();
+			string email = textBoxEmail.Text.Trim();
+			string phone = textBoxTelefonszam.Text;
+
+			if (string.IsNullOrEmpty(name))
+			{
+				throw new Exception("Név kitöltése kötelező!");
+			}
+
+			if (string.IsNullOrEmpty(email))
+			{
+				throw new Exception("Email kitöltése kötelező!");
+			}
+
+			if (string.IsNullOrEmpty(phone))
+			{
+				throw new Exception("Telefonszám kitöltése kötelező!");
+			}
+			return person;
 		}
 	}
 }
